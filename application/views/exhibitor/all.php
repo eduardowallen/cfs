d7e758ff32775a5eecf7782cdba8d3004bdb1464
Simon Kämpe <simon.kampe@gmail.com>
2013-02-26 21:24:52 +0100

    CFS-76 Search in exhibitor/all

--- application/views/exhibitor/all.php
+++ application/views/exhibitor/all.php
@@ -1,3 +1,4 @@
+<script type="text/javascript" src="js/tablesearch.js"></script>
 <h1><?php echo $headline; ?></h1>
 
 <p><a class="button add" href="user/edit/new/1"><?php echo $create_link; ?></a></p>
