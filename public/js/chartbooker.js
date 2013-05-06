5c7275377485ec667e11d4b8a031b319edc2f72a
Fredrik Yli-Loupa <andreas@trinax.se>
2013-05-06 14:00:12 +0200

    Land dropdown-lista.

--- public/js/chartbooker.js
+++ public/js/chartbooker.js
@@ -79,6 +79,7 @@ $(document).ready(function() {
 		}).done(function(reqResp){
 			var html = '<div id="popupform" style="width:500px; height:auto; padding:20px; margin:0 0 0 -250px; top:50px; height:none;"></div>';
 			var filteredResponse = $(reqResp).find('#content').html();
+
 			$('body').append(html);
 			$('#popupform').html('<img src="images/icons/close_dialogue.png" alt="" class="closeDialogue" style="margin:0 0 0 470px;"/>' + filteredResponse);
 			$('#popupform > p').css('text-align', 'left');
@@ -134,7 +135,9 @@ $(document).ready(function() {
 		e.preventDefault();
 		e.stopPropagation();
 		$('#overlay').show();
-		
+		var states = new Array("Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burma", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo, Democratic Republic", "Congo, Republic of the", "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Greenland", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, North", "Korea, South", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Mongolia", "Morocco", "Monaco", "Mozambique", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Norway", "Oman", "Pakistan", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Samoa", "San Marino", " Sao Tome", "Saudi Arabia", "Senegal", "Serbia and Montenegro", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe");
+
+
 		var url = $(this).attr('href');
 		var html = '<form action="' + url + '" method="post" id="popupform_register">'
 				 + 		'<img src="images/icons/close_dialogue.png" alt="" class="closeDialogue" style="margin:-17px 0 0 764px;"/>'
@@ -158,11 +161,13 @@ $(document).ready(function() {
 							
 				 +				'<label for="city">' + lang.city_label + ' *</label>'
 				 +				'<input type="text" name="city" id="city"/>'
-							
 				 +				'<label for="country">' + lang.country_label + ' *</label>'
-				 +				'<input type="text" name="country" id="country"/>'
-							
-				 +				'<label for="phone1">' + lang.phone1_label + ' *</label>'
+				+				'<select name="country" id="country">';
+				for(var i = 0; i<states.length; i++){
+				 html = html + '<option value="'+states[i]+'">'+states[i]+'</option>';
+				 
+				}
+				  html = html +				'</select><label for="phone1">' + lang.phone1_label + ' *</label>'
 				 +				'<input type="text" name="phone1" id="phone1"/>'
 							
 				 +				'<label for="phone2">' + lang.phone2_label + '</label>'
