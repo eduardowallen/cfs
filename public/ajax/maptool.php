e47896fbb834c6e769f2cc6c202405a8a1627241
Fredrik Yli-Loupa <andreas@trinax.se>
2013-07-05 13:40:43 +0200

    CFS-127

--- public/ajax/maptool.php
+++ public/ajax/maptool.php
@@ -450,6 +450,32 @@ if (isset($_POST['connectToFair'])) {
 	echo json_encode($response);
 }
 
+if(isset($_POST['makeComment'])){
+	$comment = new Comment;
+	session_start();
+	$me = new User;
+	$me->load($_SESSION['user_id'], 'id');
+	$author = $me->get('name');
+	$dt = $comment->set($_POST['fair'], $_POST['exhibitor'], $author, date('Y-m-d H:i:s'), $_POST['text'], $_POST['position']);
+	echo $dt;
+}
 
-
+if(isset($_POST['getComment'])){
+	$comment = new Comment;
+	$data = $comment->load($_POST['fair'], $_POST['exhibitor'], $_POST['position']);?>
+	<ul>
+	<?php foreach($data as $comments): ?>
+		<li>
+			<div class="comment">
+			<ul>
+				<li><?php echo $comments['author']?> (<?php echo $comments['date']?>) :</li>
+			</ul>
+			<ul>
+				<li><?php echo $comments['comment']?></li>
+			</ul>
+		</li>
+	<?php endforeach; ?>
+	</ul>
+	<?php
+}
 ?>
