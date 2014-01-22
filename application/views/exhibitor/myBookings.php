92e58f3753c799f9537ed46124daca0f0e61a656
Christoffer Lindahl <christoffer@trinax.se>
2014-01-22 16:57:19 +0100

    Tabellerna på /exhibitor/myBookings har fått "scrolltable"-utseendet.

--- application/views/exhibitor/myBookings.php
+++ application/views/exhibitor/myBookings.php
@@ -1,61 +1,107 @@
+<script>
+	$(document).ready(function(){
+		setTimeout(function(){
+			$('.table_set').each(function(idx, table_set) {
+				table_set = $(table_set);
+
+				$('.tblrow1', table_set).each(function(idx_cell, faux_cell) {
+					$(faux_cell).css('width', $('th', table_set).eq(idx_cell).width() + 'px');
+				});
+
+				$('table', table_set).css('margin-top', '-' + $('thead', table_set).height() + 'px');
+				$('thead', table_set).css('visibility', 'hidden');
+			});
+		}, 500);
+	});
+</script>
+
 <h1><?php echo $headline; ?></h1>
 
-<table class="std_table" style="width: 100%;">
-<thead>
-	<tr>
-		<th><?php echo $tr_fair; ?></th>
-		<th><?php echo $tr_pos; ?></th>
-		<th><?php echo $tr_area; ?> (m<sup>2</sup>)</th>
-		<th><?php echo $tr_booker; ?></th>
-		<th><?php echo $tr_field; ?></th>
-		<th><?php echo $tr_time; ?></th>
-		<th><?php echo $tr_message; ?></th>
-		<th><?php echo $tr_view; ?></th>
-	</tr>
-</thead>
-<tbody>
-<?php foreach($positions as $pos): ?>
-<?php
+<div class="table_set">
+	<div class="tblHeader">
+		<ul class="special">
+			<li><div class="tblrow1"><?php echo $tr_fair; ?></div></li>
+			<li><div class="tblrow1"><?php echo $tr_pos; ?></div></li>
+			<li><div class="tblrow1"><?php echo $tr_area; ?> (m<sup>2</sup>)</div></li>
+			<li><div class="tblrow1"><?php echo $tr_booker; ?></div></li>
+			<li><div class="tblrow1"><?php echo $tr_field; ?></div></li>
+			<li><div class="tblrow1"><?php echo $tr_time; ?></div></li>
+			<li><div class="tblrow1"><?php echo $tr_message; ?></div></li>
+			<li><div class="tblrow1"><?php echo $tr_view; ?></div></li>
+		</ul>
+	</div>
+	<div class="scrolltbl onlyfive">
+		<table class="std_table" style="float:left; padding-right: 16px;">
+			<thead>
+				<tr>
+					<th><?php echo $tr_fair; ?></th>
+					<th><?php echo $tr_pos; ?></th>
+					<th><?php echo $tr_area; ?> (m<sup>2</sup>)</th>
+					<th><?php echo $tr_booker; ?></th>
+					<th><?php echo $tr_field; ?></th>
+					<th><?php echo $tr_time; ?></th>
+					<th><?php echo $tr_message; ?></th>
+					<th><?php echo $tr_view; ?></th>
+				</tr>
+			</thead>
+			<tbody>
+<?php foreach($positions as $pos):
 
 $fair = new Fair;
 $fair->load($pos->get('exhibitor')->get('fair'), 'id');
 $maps = $fair->get('maps');
 $maps = $maps[0]->get('positions');
 ?>
-	<tr>
-		<td><?php echo $fair->get('name'); ?></td>
-		<td><?php echo $pos->get('name'); ?></td>
-		<td class="center"><?php echo $pos->get('area'); ?></td>
-		<td class="center"><?php echo $pos->get('exhibitor')->get('company'); ?></td>
-		<td class="center"><?php echo $pos->get('exhibitor')->get('commodity'); ?></td>
-		<td><?php echo date('d-m-Y H:i:s', $pos->get('exhibitor')->get('booking_time')); ?></td>
-		<td><?php echo substr($pos->get('exhibitor')->get('arranger_message'), 0, 50); ?></td>
-		<td class="center">
-			<a href="<?php echo BASE_URL.'mapTool/map/'.$pos->get('exhibitor')->get('fair').'/'.$pos->get('id').'/'.$maps[0]->map; ?>" title="<?php echo $tr_view; ?>">
-				<img src="<?php echo BASE_URL; ?>images/icons/map_go.png" alt="<?php echo $tr_view; ?>" />
-			</a>
-		</td>
-	</tr>
+				<tr>
+					<td><?php echo $fair->get('name'); ?></td>
+					<td><?php echo $pos->get('name'); ?></td>
+					<td class="center"><?php echo $pos->get('area'); ?></td>
+					<td class="center"><?php echo $pos->get('exhibitor')->get('company'); ?></td>
+					<td class="center"><?php echo $pos->get('exhibitor')->get('commodity'); ?></td>
+					<td><?php echo date('d-m-Y H:i:s', $pos->get('exhibitor')->get('booking_time')); ?></td>
+					<td><?php echo substr($pos->get('exhibitor')->get('arranger_message'), 0, 50); ?></td>
+					<td class="center">
+						<a href="<?php echo BASE_URL.'mapTool/map/'.$pos->get('exhibitor')->get('fair').'/'.$pos->get('id').'/'.$maps[0]->map; ?>" title="<?php echo $tr_view; ?>">
+							<img src="<?php echo BASE_URL; ?>images/icons/map_go.png" alt="<?php echo $tr_view; ?>" />
+						</a>
+					</td>
+				</tr>
 <?php endforeach; ?>
-</tbody>
-</table>
+			</tbody>
+		</table>
+	</div>
+</div>
 
-<h2><?php echo $rheadline; ?></h2>
+<h2 class="clear"><?php echo $rheadline; ?></h2>
 
-<table class="std_table" style="width: 100%;">
-<thead>
-	<tr>
-		<th><?php echo $tr_fair; ?></th>
-		<th><?php echo $tr_pos; ?></th>
-		<th><?php echo $tr_area; ?> (m<sup>2</sup>)</th>
-		<th><?php echo $tr_booker; ?></th>
-		<th><?php echo $tr_field; ?></th>
-		<th><?php echo $tr_time; ?></th>
-		<th><?php echo $tr_message; ?></th>
-		<th><?php echo $tr_view; ?></th>
-	</tr>
-</thead>
-<tbody>
+<div class="table_set">
+	<div class="tblHeader">
+		<ul class="special">
+			<li><div class="tblrow1"><?php echo $tr_fair; ?></div></li>
+			<li><div class="tblrow1"><?php echo $tr_pos; ?></div></li>
+			<li><div class="tblrow1"><?php echo $tr_area; ?> (m<sup>2</sup>)</div></li>
+			<li><div class="tblrow1"><?php echo $tr_booker; ?></div></li>
+			<li><div class="tblrow1"><?php echo $tr_field; ?></div></li>
+			<li><div class="tblrow1"><?php echo $tr_time; ?></div></li>
+			<li><div class="tblrow1"><?php echo $tr_message; ?></div></li>
+			<li><div class="tblrow1"><?php echo $tr_view; ?></div></li>
+		</ul>
+	</div>
+	<div class="scrolltbl onlyfive">
+		<table class="std_table" style="float:left; padding-right: 16px;">
+			<thead>
+				<tr>
+					<th><?php echo $tr_fair; ?></th>
+					<th><?php echo $tr_pos; ?></th>
+					<th><?php echo $tr_area; ?> (m<sup>2</sup>)</th>
+					<th><?php echo $tr_booker; ?></th>
+					<th><?php echo $tr_field; ?></th>
+					<th><?php echo $tr_time; ?></th>
+					<th><?php echo $tr_message; ?></th>
+					<th><?php echo $tr_view; ?></th>
+				</tr>
+			</thead>
+			<tbody>
 <?php foreach($rpositions as $pos): ?>
 <?php
 
@@ -64,43 +110,62 @@ $fair->load($pos->get('exhibitor')->get('fair'), 'id');
 $maps = $fair->get('maps');
 $maps = $maps[0]->get('positions');
 ?>
-	<tr>
-		<td><?php echo $fair->get('name'); ?></td>
-		<td><?php echo $pos->get('name'); ?></td>
-		<td class="center"><?php echo $pos->get('area'); ?></td>
-		<td class="center"><?php echo $pos->get('exhibitor')->get('company'); ?></td>
-		<td class="center"><?php echo $pos->get('exhibitor')->get('commodity'); ?></td>
-		<td><?php echo date('d-m-Y H:i:s', $pos->get('exhibitor')->get('booking_time')); ?></td>
-		<td><?php echo substr($pos->get('exhibitor')->get('arranger_message'), 0, 50); ?></td>
-		<td class="center">
-			<a href="<?php echo BASE_URL.'mapTool/map/'.$pos->get('exhibitor')->get('fair').'/'.$pos->get('id').'/'.$maps[0]->map; ?>" title="<?php echo $tr_view; ?>">
-				<img src="<?php echo BASE_URL; ?>images/icons/map_go.png" alt="<?php echo $tr_view; ?>" />
-			</a>
-		</td>
-	</tr>
+				<tr>
+					<td><?php echo $fair->get('name'); ?></td>
+					<td><?php echo $pos->get('name'); ?></td>
+					<td class="center"><?php echo $pos->get('area'); ?></td>
+					<td class="center"><?php echo $pos->get('exhibitor')->get('company'); ?></td>
+					<td class="center"><?php echo $pos->get('exhibitor')->get('commodity'); ?></td>
+					<td><?php echo date('d-m-Y H:i:s', $pos->get('exhibitor')->get('booking_time')); ?></td>
+					<td><?php echo substr($pos->get('exhibitor')->get('arranger_message'), 0, 50); ?></td>
+					<td class="center">
+						<a href="<?php echo BASE_URL.'mapTool/map/'.$pos->get('exhibitor')->get('fair').'/'.$pos->get('id').'/'.$maps[0]->map; ?>" title="<?php echo $tr_view; ?>">
+							<img src="<?php echo BASE_URL; ?>images/icons/map_go.png" alt="<?php echo $tr_view; ?>" />
+						</a>
+					</td>
+				</tr>
 <?php endforeach; ?>
-</tbody>
-</table>
+			</tbody>
+		</table>
+	</div>
+</div>
 
-<h2 style="margin-top:20px"><?php echo $prel_table; ?></h2>
-<table class="std_table" style="width: 100%;">
-<thead>
-	<tr>
-		<th><?php echo $tr_fair; ?></th>
-		<th><?php echo $tr_pos; ?></th>
-		<th><?php echo $tr_area; ?> (m<sup>2</sup>)</th>
-		<th><?php echo $tr_booker; ?></th>
-		<th><?php echo $tr_field; ?></th>
-		<th><?php echo $tr_time; ?></th>
-		<th><?php echo $tr_message; ?></th>
-		<th><?php echo $tr_view; ?></th>
-		<th><?php echo $tr_delete; ?></th>
-	</tr>
-</thead>
-<tbody>
-<?php $count = 0; ?>
-<?php foreach($prelpos as $pos): ?>
+<h2 class="clear"><?php echo $prel_table; ?></h2>
+
+<div class="table_set">
+	<div class="tblHeader">
+		<ul class="special">
+			<li><div class="tblrow1"><?php echo $tr_fair; ?></div></li>
+			<li><div class="tblrow1"><?php echo $tr_pos; ?></div></li>
+			<li><div class="tblrow1"><?php echo $tr_area; ?> (m<sup>2</sup>)</div></li>
+			<li><div class="tblrow1"><?php echo $tr_booker; ?></div></li>
+			<li><div class="tblrow1"><?php echo $tr_field; ?></div></li>
+			<li><div class="tblrow1"><?php echo $tr_time; ?></div></li>
+			<li><div class="tblrow1"><?php echo $tr_message; ?></div></li>
+			<li><div class="tblrow1"><?php echo $tr_view; ?></div></li>
+			<li><div class="tblrow1"><?php echo $tr_delete; ?></div></li>
+		</ul>
+	</div>
+	<div class="scrolltbl onlyfive">
+		<table class="std_table" style="float:left; padding-right: 16px;">
+			<thead>
+				<tr>
+					<th><?php echo $tr_fair; ?></th>
+					<th><?php echo $tr_pos; ?></th>
+					<th><?php echo $tr_area; ?> (m<sup>2</sup>)</th>
+					<th><?php echo $tr_booker; ?></th>
+					<th><?php echo $tr_field; ?></th>
+					<th><?php echo $tr_time; ?></th>
+					<th><?php echo $tr_message; ?></th>
+					<th><?php echo $tr_view; ?></th>
+					<th><?php echo $tr_delete; ?></th>
+				</tr>
+			</thead>
+			<tbody>
 <?php
+$count = 0;
+foreach($prelpos as $pos):
+
 $booking_time = $pos->get('exhibitor')->getPreliminaries($_SESSION['user_id']);
 
 $map = new FairMap;
@@ -113,27 +178,29 @@ $maps = $maps[0]->get('positions');
 //$maps = $maps[2]->get('positions');
 
 ?>
-	<tr>
-		<td><?php echo $fair->get('name'); ?></td>
-		<td><?php echo $pos->get('name'); ?></td>
-		<td class="center"><?php echo $pos->get('area'); ?></td>
-		<td class="center"><?php echo $pos->get('exhibitor')->get('company'); ?></td>
-		<td class="center"><?php echo $pos->get('exhibitor')->get('commodity'); ?></td>
-		<td class="center"><?php echo date('d-m-Y H:i:s', $booking_time[$count]['booking_time']); ?></td>
-		<td><?php echo substr($pos->get('exhibitor')->get('arranger_message'), 0, 50); ?></td>
-		<td class="center">
-			<a href="<?php echo BASE_URL.'mapTool/map/'.$booking_time[$count]['fair'].'/'.$pos->get('id').'/'.$maps[0]->map; ?>" title="<?php echo $tr_view; ?>">
-				<img src="<?php echo BASE_URL; ?>images/icons/map_go.png" alt="<?php echo $tr_view; ?>" />
-			</a>
-		</td>
-		<td class="center">
-			<a href="<?php echo BASE_URL.'exhibitor/pre_delete/'.$booking_time[$count]['id'].'/'.$_SESSION['user_id'].'/'.$booking_time[$count]['position']; ?>" title="<?php echo $tr_delete; ?>" onclick="return confirm('<?php echo $confirm_delete; ?>');">
-				<img src="<?php echo BASE_URL; ?>images/icons/delete.png" alt="<?php echo $tr_delete; ?>" />
-			</a>
-		</td>
+				<tr>
+					<td><?php echo $fair->get('name'); ?></td>
+					<td><?php echo $pos->get('name'); ?></td>
+					<td class="center"><?php echo $pos->get('area'); ?></td>
+					<td class="center"><?php echo $pos->get('exhibitor')->get('company'); ?></td>
+					<td class="center"><?php echo $pos->get('exhibitor')->get('commodity'); ?></td>
+					<td class="center"><?php echo date('d-m-Y H:i:s', $booking_time[$count]['booking_time']); ?></td>
+					<td><?php echo substr($pos->get('exhibitor')->get('arranger_message'), 0, 50); ?></td>
+					<td class="center">
+						<a href="<?php echo BASE_URL.'mapTool/map/'.$booking_time[$count]['fair'].'/'.$pos->get('id').'/'.$maps[0]->map; ?>" title="<?php echo $tr_view; ?>">
+							<img src="<?php echo BASE_URL; ?>images/icons/map_go.png" alt="<?php echo $tr_view; ?>" />
+						</a>
+					</td>
+					<td class="center">
+						<a href="<?php echo BASE_URL.'exhibitor/pre_delete/'.$booking_time[$count]['id'].'/'.$_SESSION['user_id'].'/'.$booking_time[$count]['position']; ?>" title="<?php echo $tr_delete; ?>" onclick="return confirm('<?php echo $confirm_delete; ?>');">
+							<img src="<?php echo BASE_URL; ?>images/icons/delete.png" alt="<?php echo $tr_delete; ?>" />
+						</a>
+					</td>
 
-	</tr>
+				</tr>
 <?php $count++?>
 <?php endforeach; ?>
-</tbody>
-</table>
+			</tbody>
+		</table>
+	</div>
+</div>
