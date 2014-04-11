f445c4588737dc7854143563ced0f1eb6c61ff40
Linus Ljung <linus@trinax.se>
2014-04-11 08:17:45 +0200

    Buggfix: "Options when booking" duplicerades varje gång en formuläret skickades.

--- application/models/FairExtraOption.php
+++ application/models/FairExtraOption.php
@@ -3,8 +3,8 @@
 class FairExtraOption extends Model {
 	public function load($value, $key, $value2 = NULL, $key2 = NULL) {
 		if (!is_null($key2) && !is_null($value2)) {
-			$stmt = $this->db->prepare("SELECT `id` FROM `fair_extra_option` WHERE ? = ? AND ? = ?");
-			$stmt->execute(array($key, $value, $key2, $value2));
+			$stmt = $this->db->prepare("SELECT `id` FROM `fair_extra_option` WHERE `{$key}` = ? AND `{$key2}` = ?");
+			$stmt->execute(array($value, $value2));
 			$row = $stmt->fetch(PDO::FETCH_NUM);
 
 			if (!empty($row)) {
