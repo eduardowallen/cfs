64895fa0bf8de5b6a89f9ab1ab6cd76f79486bfb
Christoffer Lindahl <christoffer@trinax.se>
2015-01-14 16:51:01 +0100

    Tog bort onödig kod i FairRegistration model.

--- application/models/FairRegistration.php
+++ application/models/FairRegistration.php
@@ -1,13 +1,4 @@
 <?php
 class FairRegistration extends Model {
-	protected $fair = null;
-	protected $author = null;
-
-	public function load($key, $by) {
-		if (parent::load($key, $by)) {
-			$this->fair = new Fair();
-			$this->fair->load($this->fair_id, 'id');
-		}
-	}
 }
 ?>
