9f27a380cdc3213ab2a30852154a606af23e34ca
Christoffer Lindahl <christoffer@trinax.se>
2015-01-15 16:28:56 +0100

    Smidigare och snabbare redirect på / till /page/loggedin och /user/login.

--- application/controllers/StartController.php
+++ application/controllers/StartController.php
@@ -1,7 +1,13 @@
 <?php
-
 class StartController extends Controller {
-	
-}
+	public function index() {
+		if (isset($_SESSION['user_id'])) {
+			header('Location: ' . BASE_URL . 'page/loggedin');
+		} else {
+			header('Location: ' . BASE_URL . 'user/login');
+		}
 
+		die();
+	}
+}
 ?>
