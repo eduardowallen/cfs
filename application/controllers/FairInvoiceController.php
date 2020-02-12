8cb93572e050cf6f45baad6295f31667d8a5d532
Eduardo <eduardo.wallen@gmail.com>
2020-02-12 14:14:44 +0100

    The functions were in the wrong place...

--- application/controllers/FairInvoiceController.php
+++ application/controllers/FairInvoiceController.php
@@ -3,7 +3,7 @@ class FairInvoiceController extends Controller {
 
 	function delete_invoice($row_id) {
 		setAuthLevel(4);
-        $invoice_to_delete = new FairInvoice();
+        $invoice_to_delete = new ExhibitorInvoice();
         $invoice_to_delete->load($row_id, 'row_id');
         if ($invoice_to_delete->wasLoaded())
             error_log('Invoice was loaded in FairInvoiceController.php on line 8.');
--- application/models/ExhibitorInvoice.php
+++ application/models/ExhibitorInvoice.php
@@ -21,7 +21,28 @@ class ExhibitorInvoice extends Model {
 			return false;
 		}
 	}
-	
+	public function perm_delete() {
+		if ($this->wasLoaded()) {
+			error_log('Invoice was loaded in FairInvoice.php on line 24.');
+			$exhibitor = new Exhibitor();
+			$exhibitor->load2($this->exhibitor, 'id');
+			if ($exhibitor->wasLoaded()) {
+				error_log('Exhibitor was loaded in FairInvoice.php on line 28.');
+				$position = new FairMapPosition();
+				$position->load2($exhibitor->get('position'), 'id');
+				if ($position->wasLoaded()) {
+					error_log('Position was loaded in FairInvoice.php on line 32.');
+					$invoice_file = ROOT.'public/invoices/fairs/'.$fairId.'/exhibitors/'.$this->exhibitor.'/'.$rec_billing_company_name . '-' . $position->get('name'). '-' . $this->id . '.pdf';
+					rm($invoice_file);
+					error_log('Invoice was removed in FairInvoiceController.php on line 35.');
+					$stmt = $this->db->prepare("DELETE FROM exhibitor_invoice_rel WHERE invoice = ? AND fair = ?");
+					$stmt->execute($this->id, $this->fair);
+					$stmt = $this->db->prepare("DELETE FROM exhibitor_invoice WHERE row_id = ?");
+					$stmt->execute($this->row_id);
+				} else error_log("The position could not be loaded");
+			} else error_log("The exhibitor could not be loaded");
+		} else error_log("The file with row id ".$this->row_id." could not be deleted.");
+	}
 }
 
 ?>
